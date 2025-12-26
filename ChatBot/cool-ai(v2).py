33824a1350f7f9a3051902e8f3e2babb777550b7
puniiith25 <puniiith25@gmail.com>
2025-12-26 20:33:45 +0530

    gemini link

--- ChatBot/cool-ai(v2).py
+++ ChatBot/cool-ai(v2).py
@@ -5,3 +5,5 @@ import requests
 
 API_KEY = "AIzaSyCvvkKuAMoQNrXFxsHbpUysKwTIjj_eOhw"
 
+
+url = "https://generativelanguage.googleapis.com/v1/models/gemini-2.5-flash:generateContent?key=" + API_KEY
