9de57a8c585be5d011a2bd8c45a3434b5d859f71
puniiith25 <puniiith25@gmail.com>
2025-12-26 20:33:58 +0530

    code

--- ChatBot/cool-ai(v2).py
+++ ChatBot/cool-ai(v2).py
@@ -7,3 +7,25 @@ API_KEY = "AIzaSyCvvkKuAMoQNrXFxsHbpUysKwTIjj_eOhw"
 
 
 url = "https://generativelanguage.googleapis.com/v1/models/gemini-2.5-flash:generateContent?key=" + API_KEY
+
+prompt = input(str("Chat-bot:"))
+
+body = {
+    "contents": [
+        {
+            "parts": [
+                {"text": prompt}
+            ]
+        }
+    ]
+}
+
+response = requests.post(url, json=body)
+
+if response.status_code == 200:
+    data = response.json()
+    text = data["candidates"][0]["content"]["parts"][0]["text"]
+    print("\nResponse:\n")
+    print(text)
+else:
+    print("Error:", response.text)
